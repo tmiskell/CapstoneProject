b9fff04bc5ae22841aeefe28b0bf8822f011cf4a
tmiskell <timothymiskell@gmail.com>
2016-04-09 20:06:05 +0000

    Updated start and stop script to handle latest changes to RPi2 setup.

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
+#/bin/sh
 # Setup directories.
+export BASE_DIR=/home/$USER/CapstoneProject
 export GESTURE_DIR=gesture_data
 export CONVERT_DIR=microcomputer
 export I2C_DIR=microcontroller
@@ -18,22 +20,51 @@ UP_DEL=125
 RD_DEL=100
 # Setup the IP address for the server.
 IP_ADDR=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-#IP_ADDR=127.0.0.1
+if ! [[ -z "$IP_ADDR" ]] ; then
+  # No internet connection
+  IP_ADDR=127.0.0.1
+fi
+# Allow time for desktop environment to setup.
+sleep 5
 # Configure the GPIO pins.
 echo "Configuring GPIO pins"
 gpio export 27 out
 echo 1 > /sys/class/gpio/gpio27/value
 sleep 1
+# Change to base directory
+cd $BASE_DIR
 # Start I2C transfers.
 echo "Starting I2C transfers from sensors"
 $I2C_DIR/i2c_transfer $IN_LB $IN_UB $MID_LB $MID_UB $RI_LB $RI_UB $PI_LB $PI_UB $TH_LB $TH_UB $UP_DEL $RD_DEL > $LOG_DIR/transfer.log 2> $LOG_DIR/transfer_error.log &
 sleep 1
-# Start the web server.
-echo "Starting web server"
-$GESTURE_DIR/server.py $IP_ADDR > $LOG_DIR/server.log 2> $LOG_DIR/server_error.log &
-sleep 1
 # Start sign to speech conversion.
 cd $CONVERT_DIR
-echo "Starting sign to speech conversion"
+gnome-terminal -e ./sign2speech
 sleep 1
-./sign2speech
+# Return to base directory and perform clean up.
+cd $BASE_DIR
+# Stop sign to speech conversion.
+PID=$(pgrep sign2speech)
+if ! [[ -z "$PID" ]] ; then
+  echo "Stopping sign to speech conversion."
+  kill $PID
+  sleep 1
+fi
+# Stop I2C transfers.
+PID=$(pgrep i2c_transfer)
+if ! [[ -z "$PID" ]] ; then
+  echo "Stopping I2C transfers from sensors"
+  kill $PID
+  sleep 1
+fi
+# Clean up GPIO pin configuration.
+echo "Cleaning up GPIO pin configuration"
+gpio unexportall
+# Clean up XML files.
+if [ -f $GESTURE_DIR/gesture_data_init.xml ] ; then
+  echo "Cleaning up XML files"
+  rm -v $GESTURE_DIR/gesture_data_init.xml
+fi
+# Request shutdown
+echo "Requesting shutdown"
+echo "shutdown" > /tmp/shutdown_request.txt
